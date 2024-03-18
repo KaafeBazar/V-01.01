9403209e0f2a9f2b30c5786c4eec52db053cf80c
Ehsan Hossein Khani <144338242+Ehsan6452@users.noreply.github.com>
2024-03-18 12:06:54 +0330

    Update Home-Script.js

--- js/Home/Home-Script.js
+++ js/Home/Home-Script.js
@@ -1,49 +1,3 @@
-function TabFocuse (tab)
-{
-// Get Tabs
-var TabHome = document.getElementById("TabHome");
-var TabCategory = document.getElementById("TabCategory");
-var TabWebLog = document.getElementById("TabWebLog");
-var TabTraining = document.getElementById("TabTraining");
-// Switch on tab 
-
-switch(tab)
-    {
-        case "Home":
-            {
-                TabHome.classList.add("TabActive");
-                TabCategory.classList.remove("TabActive");
-                TabWebLog.classList.remove("TabActive");
-                TabTraining.classList.remove("TabActive");
-                break;
-            }
-        case "Category":
-            {
-                TabHome.classList.remove("TabActive");
-                TabCategory.classList.add("TabActive");
-                TabWebLog.classList.remove("TabActive");
-                TabTraining.classList.remove("TabActive");
-                break;
-            }
-        case "WebLog":
-            {
-                TabHome.classList.remove("TabActive");
-                TabCategory.classList.remove("TabActive");
-                TabWebLog.classList.add("TabActive");
-                TabTraining.classList.remove("TabActive");
-                break;
-            }
-        case "Training":
-            {
-                TabHome.classList.remove("TabActive");
-                TabCategory.classList.remove("TabActive");
-                TabWebLog.classList.remove("TabActive");
-                TabTraining.classList.add("TabActive");
-                break;
-            }
-
-    }
-}
 function ChangeAccountIcon(pos)
 {
     var icon =document.getElementById('Account_icon');
@@ -62,3 +16,63 @@ function ChangeAccountIcon(pos)
         }
 }
 
+function TabFocuse (tab)
+{
+// Get Tabs
+var TabHome = document.getElementById("TabHome");
+var TabCategory = document.getElementById("TabCategory");
+var TabWebLog = document.getElementById("TabWebLog");
+var TabTraining = document.getElementById("TabTraining");
+var TabAboutUs = document.getElementById("TabAboutUs");
+// Switch on tab 
+
+switch(tab)
+    {
+        case "Home":
+            {
+                TabHome.classList.add("TabActive");
+                TabCategory.classList.remove("TabActive");
+                TabWebLog.classList.remove("TabActive");
+                TabTraining.classList.remove("TabActive");
+                TabAboutUs.classList.remove("TabActive");
+                break;
+            }
+        case "Category":
+            {
+                TabHome.classList.remove("TabActive");
+                TabCategory.classList.add("TabActive");
+                TabWebLog.classList.remove("TabActive");
+                TabTraining.classList.remove("TabActive");
+                TabAboutUs.classList.remove("TabActive");
+                break;
+            }
+        case "WebLog":
+            {
+                TabHome.classList.remove("TabActive");
+                TabCategory.classList.remove("TabActive");
+                TabWebLog.classList.add("TabActive");
+                TabTraining.classList.remove("TabActive");
+                TabAboutUs.classList.remove("TabActive");
+                break;
+            }
+        case "Training":
+            {
+                TabHome.classList.remove("TabActive");
+                TabCategory.classList.remove("TabActive");
+                TabWebLog.classList.remove("TabActive");
+                TabTraining.classList.add("TabActive");
+                TabAboutUs.classList.remove("TabActive");
+                break;
+            }
+        
+        case "AboutUs":
+            {
+                TabHome.classList.remove("TabActive");
+                TabCategory.classList.remove("TabActive");
+                TabWebLog.classList.remove("TabActive");
+                TabTraining.classList.remove("TabActive");
+                TabAboutUs.classList.add("TabActive");
+                break;
+            }
+    }
+}
